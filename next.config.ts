4560b46b29fa1a62a278e978cc88a27926e7e563
jzarcoo <zarco_romero_jose_antonio@comunidad.unam.mx>
2024-12-05 13:24:38 -0600

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: 'export',
+  output: 'export',
+  basePath: '/zarco',
+  assetPrefix: isProd ? '/zarco' : ''
 };
 
 export default nextConfig;
